c079c4eff38a48f671237be3e50e4f757295c4bc
ManuelFrigeri <manuel@gronda.eu>
2020-11-26 15:24:19 +0100

    Added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 
 require('./models/User')
 require('./services/passport')
@@ -9,11 +10,20 @@ require('./services/passport')
 
 const keys = require('./config/keys')
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets like our main.js file, or main.css
+    app.use(express.static('client/build'))
+    //Express will serve up the index.html file if it doesn't recognized the route
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(keys.mongoURI)
 const app = express()
-
+app.use(bodyParser.json())
 app.use(
     cookieSession({
         maxAge: 30*24*60*60*1000,
@@ -29,7 +39,7 @@ app.use(passport.session())
 
 
 require('./routes/authRoutes')(app) // it is like function (parameters)
-
+require('./routes/billingRoutes')(app)
 
 
 
