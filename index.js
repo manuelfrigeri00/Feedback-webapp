54805552773ca17f452d69f2ffe1c7fb181eb875
ManuelFrigeri <manuel@gronda.eu>
2020-11-26 15:44:03 +0100

    added some fixes

--- index.js
+++ index.js
@@ -6,23 +6,15 @@ const bodyParser = require('body-parser')
 
 require('./models/User')
 require('./services/passport')
-
-
 const keys = require('./config/keys')
 
-if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets like our main.js file, or main.css
-    app.use(express.static('client/build'))
-    //Express will serve up the index.html file if it doesn't recognized the route
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
-const PORT = process.env.PORT || 5000
-
 mongoose.connect(keys.mongoURI)
+
 const app = express()
+
+
+
+
 app.use(bodyParser.json())
 app.use(
     cookieSession({
@@ -36,13 +28,24 @@ app.use(passport.session())
 
 
 
-
-
 require('./routes/authRoutes')(app) // it is like function (parameters)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
+const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
     console.log('connected on port:' + PORT)
 })
